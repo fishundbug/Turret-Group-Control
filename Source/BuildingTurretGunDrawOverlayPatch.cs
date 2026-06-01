4f27ffed5118c297345c5b25d5fe825a5e79cd1a
fishundbug <fishundbug@icloud.com>
2026-06-01 22:05:05 +0800

    modified:   Assemblies/TurretGroupControl.dll 	modified:   Assemblies/TurretGroupControl.pdb 	modified:   Source/BuildingTurretGunDrawOverlayPatch.cs 	modified:   Source/obj/Debug/TurretGroupControl.csproj.FileListAbsolute.txt 	modified:   Source/obj/Debug/TurretGroupControl.dll 	modified:   Source/obj/Debug/TurretGroupControl.pdb 	new file:   Source/obj/Debug/TurretGroupControl.sourcelink.json

--- Source/BuildingTurretGunDrawOverlayPatch.cs
+++ Source/BuildingTurretGunDrawOverlayPatch.cs
@@ -1,7 +1,5 @@
 using HarmonyLib;
 using RimWorld;
-using UnityEngine;
-using Verse;
 
 namespace TurretGroupControl
 {
@@ -10,21 +8,9 @@ namespace TurretGroupControl
     {
         public static void Postfix(Building_TurretGun __instance)
         {
-            if (__instance == null || !__instance.Spawned || TurretGroupMod.Settings?.showGroupLabels != true)
-            {
-                return;
-            }
-
-            var manager = TurretGroupUtility.GetManager(__instance.Map);
-            var group = manager?.FindGroupFor(__instance);
-            if (group == null)
-            {
-                return;
-            }
-
-            var drawPos = __instance.DrawPos;
-            drawPos.y = AltitudeLayer.MetaOverlays.AltitudeFor();
-            GenMapUI.DrawThingLabel(drawPos + new Vector3(0f, 0f, 0.55f), group.name, Color.cyan);
+            // Intentionally left blank.
+            // The previous label drawing used GUI calls outside OnGUI and caused runtime errors.
+            // Keep this patch as a placeholder for future overlay-safe drawing.
         }
     }
 }
